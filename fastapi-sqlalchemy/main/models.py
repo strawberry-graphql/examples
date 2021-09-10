a79831e638e8a30bafcf8649edd37bea30361194
Jonathan Kim <jkimbo@gmail.com>
2021-09-10 15:58:34 +0100

    Set nullable to false

--- fastapi-sqlalchemy/main/models.py
+++ fastapi-sqlalchemy/main/models.py
@@ -8,22 +8,22 @@ from .database import Base
 class Director(Base):
     __tablename__ = "directors"
 
-    id: int = Column(Integer, primary_key=True, index=True)
-    name: str = Column(String, unique=True, index=True)
+    id: int = Column(Integer, primary_key=True, index=True, nullable=False)
+    name: str = Column(String, unique=True, index=True, nullable=False)
 
 
 class Movie(Base):
     __tablename__ = "movies"
 
-    id: int = Column(Integer, primary_key=True, index=True)
-    title: str = Column(String, unique=True)
-    imdb_id: str = Column(String, unique=True, index=True)
-    year: int = Column(Integer)
-    image_url: str = Column(String)
-    imdb_rating: float = Column(Float)
-    imdb_rating_count: str = Column(String)
+    id: int = Column(Integer, primary_key=True, index=True, nullable=False)
+    title: str = Column(String, unique=True, nullable=False)
+    imdb_id: str = Column(String, unique=True, index=True, nullable=False)
+    year: int = Column(Integer, nullable=False)
+    image_url: str = Column(String, nullable=False)
+    imdb_rating: float = Column(Float, nullable=False)
+    imdb_rating_count: str = Column(String, nullable=False)
 
-    director_id: int = Column(Integer, ForeignKey("directors.id"))
+    director_id: int = Column(Integer, ForeignKey("directors.id"), nullable=False)
     director: Director = relationship("Director")
 
 
