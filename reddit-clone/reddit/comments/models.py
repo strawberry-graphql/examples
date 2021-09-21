9ce38ab51ebbf909fbdf08d81bead702222bd64b
Aryan Iyappan <aryaniyappan2006@gmail.com>
2021-09-21 11:47:15 +0530

    add alembic skeleton

--- reddit-clone/reddit/comments/models.py
+++ reddit-clone/reddit/comments/models.py
@@ -1,3 +1,8 @@
+from typing import Optional
+
+from sqlalchemy import Column, Integer, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
 from reddit.core.models import BaseModel
 
 
@@ -5,3 +10,32 @@ class Comment(BaseModel):
     """
     Represents a Comment in a thread.
     """
+
+    content: str = Column(
+        Text,
+        nullable=False,
+        comment="""
+        The content of the comment.
+        """,
+    )
+
+    votes: int = Column(
+        Integer,
+        default=1,
+        comment="""
+        The votes for the thread.
+        """,
+    )
+
+    user_id: Optional[int] = Column(
+        Integer,
+        ForeignKey("users.id"),
+        comment="""
+        The owner ID of the comment.
+        """,
+    )
+
+    replies = relationship("Comment", backref="parent", lazy="dynamic")
+
+    def __repr__(self) -> str:
+        return "<Comment %s>" % self.id
