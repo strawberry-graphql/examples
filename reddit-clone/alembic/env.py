900d2597f739de8ecf4615535c8417b83996fce8
Aryan Iyappan <69184573+codebyaryan@users.noreply.github.com>
2021-09-25 17:01:14 +0530

    Update env.py

--- reddit-clone/alembic/env.py
+++ reddit-clone/alembic/env.py
@@ -13,11 +13,9 @@ config = context.config
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from reddit.db.base import Base # noqa
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
