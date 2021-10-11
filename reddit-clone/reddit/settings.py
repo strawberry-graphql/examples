bc586951a2617ed43ac3ca7eace1017fc036b662
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-11 11:23:36 +0530

    describe mail config values

--- reddit-clone/reddit/settings.py
+++ reddit-clone/reddit/settings.py
@@ -11,9 +11,17 @@ DEBUG: bool = config("DEBUG", cast=bool, default=False)
 # SQLAlchemy database URL.
 DATABASE_URI: str = config("DATABASE_URI", cast=str)
 
-# mail client configuration.
+# mail client host name.
 MAIL_HOST: str = config("MAIL_HOST", cast=str)
+
+# mail client port.
 MAIL_PORT: int = config("MAIL_PORT", cast=int)
+
+# mail client auth username.
 MAIL_USERNAME: Optional[str] = config("MAIL_USERNAME", cast=str, default=None)
+
+# mail client auth password.
 MAIL_PASSWORD: Optional[str] = config("MAIL_PASSWORD", cast=str, default=None)
+
+# mail client sender address.
 MAIL_SENDER: Optional[str] = config("MAIL_SENDER", cast=str, default=None)
