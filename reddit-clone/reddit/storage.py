e17eb2844de14ffcfe0228ff431267507fdd5cdf
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-11 09:00:07 +0530

    update storage helper: generate-file-name

--- reddit-clone/reddit/storage.py
+++ reddit-clone/reddit/storage.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from hashlib import sha256
 
 import aiofiles
 
@@ -9,6 +10,7 @@ def generate_file_name(file_name: str) -> str:
     """
     Generates a secure file name for the given file.
     """
+    return sha256(file_name.encode("utf-8")).hexdigest()
 
 
 async def save_file(file, path: Path) -> None:
