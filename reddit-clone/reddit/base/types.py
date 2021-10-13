feb613e630d06ca484c873d4b8d74a91b7bfb50a
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-13 11:17:28 +0530

    update node type

--- reddit-clone/reddit/base/types.py
+++ reddit-clone/reddit/base/types.py
@@ -20,16 +20,16 @@ class NodeType:
         cls, info: Info, global_id: str, only_type=None
     ) -> Optional[Type[NodeType]]:
         try:
-            _type, _id = cls.from_global_id(global_id)
+            type_name, _id = cls.from_global_id(global_id)
         except Exception as e:
             raise Exception(
                 f'Unable to parse global ID "{global_id}". '
                 f"Exception message: {str(e)}"
             )
 
-        schema_type = info.schema.get_type(_type)
+        schema_type = info.schema.get_type_by_name(type_name)
         if schema_type is None:
-            raise Exception(f'Relay Node "{_type}" not found in schema')
+            raise Exception(f'Relay Node "{type_name}" not found in schema')
 
         if only_type:
             assert schema_type == only_type
@@ -37,7 +37,7 @@ class NodeType:
         # We make sure the ObjectType implements the "Node" interface
         if cls not in schema_type._type_definition.interfaces:
             raise Exception(
-                f'ObjectType "{_type}" does not implement the "{cls}" interface.'
+                f'ObjectType "{type_name}" does not implement the "{cls}" interface.'
             )
 
         resolver = getattr(schema_type, "resolve_node", None)
