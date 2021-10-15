7f2bb23c107827a90baef6d0a7dfb71a7881c658
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-15 12:05:58 +0530

    update tasks.py

--- reddit-clone/reddit/tasks.py
+++ reddit-clone/reddit/tasks.py
@@ -7,6 +7,3 @@ __all__ = ("celery",)
 celery = Celery(__name__)
 celery.conf.result_backend = settings.CELERY_BACKEND
 celery.conf.broker_url = settings.CELERY_BROKER
-
-if __name__ == "__main__":
-    celery.start()
